a658fc48ed597d26527a739b4eecb4c2f400cc64
magmadog <36441904+magmadog@users.noreply.github.com>
2020-01-28 21:22:35 +0300

    delete some files

--- app/src/main/java/com/example/petrsumobile/ui/Interface/ItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.petrsumobile.ui.Interface;
-
-import android.view.View;
-
-public interface ItemClickListener {
-    void onClick(View view, int position, boolean isLongClick);
-}
