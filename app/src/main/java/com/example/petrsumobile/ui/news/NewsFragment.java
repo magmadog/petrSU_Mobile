a77ce08b923c7f299d930b7ce7c440759b325ba3
magmadog <36441904+magmadog@users.noreply.github.com>
2020-01-28 21:32:16 +0300

    some fixes

--- app/src/main/java/com/example/petrsumobile/ui/news/NewsFragment.java
+++ app/src/main/java/com/example/petrsumobile/ui/news/NewsFragment.java
@@ -73,8 +73,8 @@ public class NewsFragment extends Fragment {
     {
         try
         {
-            //openConnection() returns instance that represents a connection to the remote object referred to by the URL
-            //getInputStream() returns a stream that reads from the open connection
+            //openConnection()
+            //getInputStream()
             return url.openConnection().getInputStream();
         }
         catch (IOException e)
@@ -99,71 +99,48 @@ public class NewsFragment extends Fragment {
 
             try
             {
-                URL url = new URL("http://feeds.news24.com/articles/fin24/tech/rss");
+                URL url = new URL("https://petrsu.ru/rss?type=1");
 
-                //creates new instance of PullParserFactory that can be used to create XML pull parsers
                 XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-
-                //Specifies whether the parser produced by this factory will provide support
-                //for XML namespaces
                 factory.setNamespaceAware(false);
-
-                //creates a new instance of a XML pull parser using the currently configured
-                //factory features
                 XmlPullParser xpp = factory.newPullParser();
 
-                // We will get the XML from an input stream
                 xpp.setInput(getInputStream(url), "UTF_8");
 
-                /* We will parse the XML content looking for the "<title>" tag which appears inside the "<item>" tag.
-                 * We should take into consideration that the rss feed name is also enclosed in a "<title>" tag.
-                 * Every feed begins with these lines: "<channel><title>Feed_Name</title> etc."
-                 * We should skip the "<title>" tag which is a child of "<channel>" tag,
-                 * and take into consideration only the "<title>" tag which is a child of the "<item>" tag
-                 *
-                 * In order to achieve this, we will make use of a boolean variable called "insideItem".
-                 */
                 boolean insideItem = false;
 
-                // Returns the type of current event: START_TAG, END_TAG, START_DOCUMENT, END_DOCUMENT etc..
                 int eventType = xpp.getEventType(); //loop control variable
 
                 while (eventType != XmlPullParser.END_DOCUMENT)
                 {
-                    //if we are at a START_TAG (opening tag)
                     if (eventType == XmlPullParser.START_TAG)
                     {
-                        //if the tag is called "item"
                         if (xpp.getName().equalsIgnoreCase("item"))
                         {
                             insideItem = true;
                         }
-                        //if the tag is called "title"
                         else if (xpp.getName().equalsIgnoreCase("title"))
                         {
                             if (insideItem)
                             {
-                                // extract the text between <title> and </title>
                                 titles.add(xpp.nextText());
                             }
                         }
-                        //if the tag is called "link"
                         else if (xpp.getName().equalsIgnoreCase("link"))
                         {
                             if (insideItem)
                             {
-                                // extract the text between <link> and </link>
                                 links.add(xpp.nextText());
                             }
                         }
                     }
-                    //if we are at an END_TAG and the END_TAG is called "item"
+
                     else if (eventType == XmlPullParser.END_TAG && xpp.getName().equalsIgnoreCase("item"))
                     {
                         insideItem = false;
                     }
 
-                    eventType = xpp.next(); //move to next element
+                    eventType = xpp.next();
                 }
 
 
